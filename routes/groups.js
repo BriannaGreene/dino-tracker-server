9789b536c81407d65946fec6ed800f68932e37fd
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-07 14:40:02 -0700

    seeds working

--- routes/groups.js
+++ routes/groups.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const knex = require('knex')
+// const db = require('../db')
+const knex = require('../knex')
+
+
 
 router.get('/', (req, res, next) => {
   // code goes here
