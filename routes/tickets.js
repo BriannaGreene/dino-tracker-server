7421c2a17157ad931a44d28785497ddbceb4b51c
Brianna Greene <brianna.greene.designs@gmail.com>
2018-01-10 11:32:22 -0700

    added ticket route to get tickets for current user

--- routes/tickets.js
+++ routes/tickets.js
@@ -3,18 +3,23 @@ var router = express.Router()
 const knex = require('../knex')
 
 router.get('/', (req, res, next) => {
+  console.log('HITTING TICKET ROUTE');
+  const user = req.params
+  // console.log(req.params);
   // code goes here
   knex('tickets')
     .select('*')
     .orderBy('id')
     .then(data => {
+      console.log('DATA FROM TICKETS: ', data);
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify(data))
     })
-  // .catch((err) => next(err))
+  .catch((err) => next(err))
 })
 
 router.get('/:id', (req, res, next) => {
+  console.log('woops wrong route');
   const id = req.params.id
   // code goes here
   knex('tickets')
@@ -23,6 +28,26 @@ router.get('/:id', (req, res, next) => {
     .then(data => {
       res.setHeader('Content-Type', 'application/json')
       res.send(JSON.stringify(data))
+    })
+})
+
+router.get('/user/:id', (req, res, next) => {
+  // refactor route to use user id instead of 2!!!!!
+  const id = req.params.id
+  console.log('USER ID: ', id);
+  knex('tickets')
+    .select('*')
+    .then(data => {
+      let userTickets = data.filter(obj => {
+        let assignees = obj.assignees.assignees
+        for (var i = 0; i < assignees.length; i++) {
+          if (assignees[i] === 2) {
+            return assignees[i]
+          }
+        }
+      })
+      res.setHeader('Content-Type', 'application/json')
+      res.send(JSON.stringify(userTickets))
     })
 })
 
